88ef86222ce35ee7fa1a4f709b188ead4a0850e0
furkancs87 <84360772+furkancs87@users.noreply.github.com>
2025-03-03 18:12:28 +0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import CoreConcept from './components/CoreConcept.jsx';
 import TabButton from './components/TabButton.jsx';
 import { useState } from 'react';
 import { EXAMPLES } from './data.js';
-//BU BİR DENEMEDİR1
+//BU BİR DENEMEDİR
 /*
 function CoreConcept(props) {
   return (
